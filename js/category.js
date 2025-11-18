fa3990abeb1171ad0b63281add7a93e90a05e004
Bernardita Galdames <bgaldames@udesa.edu.ar>
2025-11-18 00:04:25 -0300

    agregue alerts formulario y modifique headers

---
+++ js/category.js
@@ -0,0 +1,22 @@
+//header//
+let formulario = document.querySelector('.formHeader');
+let buscador = document.querySelector('.buscarHeader');
+let boton = document.querySelector('.botonHeader');
+
+buscador.addEventListener ('input', function () {
+
+})
+
+formulario.addEventListener ('submit', function(e) {
+    e.preventDefault();
+
+    if (buscador.value === "") {
+        alert ("El campo de busqueda no puede estar vacío.")
+        return;
+    }
+    if (buscador.value.length < 3) {
+        alert("El término debe tener al menos 3 caracteres")
+        return;
+    }
+    this.submit()
+})
--- js/login.js
+++ js/login.js
@@ -1,3 +1,27 @@
+//header//
+let formulario = document.querySelector('.formHeader');
+let buscador = document.querySelector('.buscarHeader');
+let boton = document.querySelector('.botonHeader');
+
+buscador.addEventListener ('input', function () {
+
+})
+
+formulario.addEventListener ('submit', function(e) {
+    e.preventDefault();
+
+    if (buscador.value === "") {
+        alert ("El campo de busqueda no puede estar vacío.")
+        return;
+    }
+    if (buscador.value.length < 3) {
+        alert("El término debe tener al menos 3 caracteres")
+        return;
+    }
+    this.submit()
+})
+
+//form iniciar sesion// 
 let form = document.querySelector(".formulario");
 let email = document.querySelector(".email");
 let password = document.querySelector(".password");
