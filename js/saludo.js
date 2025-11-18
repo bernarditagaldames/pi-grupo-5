28b071d7273ae748f9735ca48f80aaa8a5159738
Barbara Lladhon <blladhon@udesa.edu.ar>
2025-11-18 13:34:48 -0300

    hice saludo.js, linkee todo, modifique html

---
+++ js/saludo.js
@@ -0,0 +1,28 @@
+let usuarioGuardado = localStorage.getItem("emailUsuario");
+
+let textoBienvenida = document.querySelector(".bienvenida"); 
+let linkLogout = document.querySelector(".logoutLink");
+
+let linkLogin = document.querySelector(".loginLink");
+let linkRegister = document.querySelector(".registerLink");
+
+if (usuarioGuardado != null) {
+
+    if (textoBienvenida != null) {
+        textoBienvenida.innerText = "Bienvenido: " + usuarioGuardado;
+        textoBienvenida.style.display = "inline";
+    }
+
+    if (linkLogout != null) {
+        linkLogout.style.display = "inline";
+    }
+
+    if (linkLogin != null) {
+        linkLogin.style.display = "none";
+    }
+
+    if (linkRegister != null) {
+        linkRegister.style.display = "none";
+    }
+
+}
